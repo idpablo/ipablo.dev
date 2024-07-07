efd6c00a8da2896e8422a376246a4e7230094046
Pablo Bento Soares <pbs.97@outlook.com>
2024-07-07 14:27:07 -0300

    Corrigindo bug acusado pelo sonar

--- src/theme/ThemeContext.tsx
+++ src/theme/ThemeContext.tsx
@@ -1,14 +1,24 @@
-import React, { createContext, useState, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode, useEffect } from 'react';
+
+interface ThemeContextType {
+  theme: string;
+  setTheme: (theme: string) => void;
+}
 
 interface ThemeProviderProps {
   children: ReactNode;
 }
 
-const ThemeContext = createContext<any>(null);
+const defaultTheme = 'light';
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const storedTheme = localStorage.getItem('theme' || 'light');
+  const storedTheme = localStorage.getItem('theme') || defaultTheme;
   const [theme, setTheme] = useState(storedTheme);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
