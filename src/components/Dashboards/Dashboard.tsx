acfe576b411a547861f21d6f31ee43752f272bb3
Pablo Bento Soares <pbs.97@outlook.com>
2024-07-07 16:04:44 -0300

    Adicionando modelo de exibição de projetos
    O portifólio vai ter como ponto de referencia de projetos os repositórios públicos no GitHub.
    
    - Melhorar descrição dos repositorios
    - Incluir Imagens para renderizar no site.

--- src/components/Dashboards/Dashboard.tsx
+++ src/components/Dashboards/Dashboard.tsx
@@ -1,47 +1,51 @@
-import React from "react";
-
-import { DashboardContainer, DashboardProjects, DashboardInfo, DashboardCard } from '../../theme/Theme';
+import React, { useEffect, useState } from "react";
+import { DashboardContainer, DashboardProjects, DashboardInfo, DashboardCard, LinkStyled } from '../../theme/Theme';
 
 const Dashboard: React.FC = () => {
-    return (
-      <DashboardContainer>
-        <DashboardProjects>
-        <DashboardCard>
-              <h4>Project 1</h4>
-              <br></br>
-              <p>O Innovative Learning Portal (ILP) é uma plataforma educacional avançada projetada para transformar a experiência de aprendizado online. Combinando tecnologias de ponta, personalização inteligente e uma interface amigável, o ILP visa criar um ambiente de aprendizado dinâmico e interativo para estudantes de todas as idades e disciplinas.</p>
+  const [projects, setProjects] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchGitHubProjects = async () => {
+      try {
+        const response = await fetch("https://api.github.com/users/idpablo/repos");
+        if (response.ok) {
+          const data = await response.json();
+          setProjects(data);
+        } else {
+          console.error("Erro ao buscar projetos do GitHub:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar projetos do GitHub:", error);
+      }
+    };
+
+    fetchGitHubProjects();
+  }, []);
+
+  return (
+    <DashboardContainer>
+      <DashboardProjects>
+        {projects.map((project) => (
+          <DashboardCard key={project.id}>
+            <h4>{project.name}</h4>
+            <br />
+            <p>{project.description}</p>
+            <LinkStyled href={project.html_url} target="_blank" rel="noopener noreferrer">Ver no GitHub</LinkStyled>
           </DashboardCard>
-          <DashboardCard>
-              <h4>Project 2</h4>
-              <br></br>
-              <p>O Innovative Learning Portal (ILP) é uma plataforma educacional avançada projetada para transformar a experiência de aprendizado online. Combinando tecnologias de ponta, personalização inteligente e uma interface amigável, o ILP visa criar um ambiente de aprendizado dinâmico e interativo para estudantes de todas as idades e disciplinas.</p>
-          </DashboardCard>
-          <DashboardCard>
-              <h4>Project 3</h4>
-              <br></br>
-              <p>O Innovative Learning Portal (ILP) é uma plataforma educacional avançada projetada para transformar a experiência de aprendizado online. Combinando tecnologias de ponta, personalização inteligente e uma interface amigável, o ILP visa criar um ambiente de aprendizado dinâmico e interativo para estudantes de todas as idades e disciplinas.</p>
-          </DashboardCard>
-          <DashboardCard>
-              <h4>Project 4</h4>
-              <br></br>
-              <p>O Innovative Learning Portal (ILP) é uma plataforma educacional avançada projetada para transformar a experiência de aprendizado online. Combinando tecnologias de ponta, personalização inteligente e uma interface amigável, o ILP visa criar um ambiente de aprendizado dinâmico e interativo para estudantes de todas as idades e disciplinas.</p>
-          </DashboardCard>
-        </DashboardProjects>
-        <DashboardInfo>
-          <h2>Meu Nome é Pablo Bento Soares</h2>
-          <br></br>
-          <p>
-              Sou um desenvolvedor de software que teve o início da carreira na infraestrutura, experiência em scripts de automação, servidores e estruturas de escalabilidade e monitoramento, dando ênfase nas linguagens; Java, Python e Java Script.
-              Uma pessoa que ama a tecnologia desde que me entendo por gente, sempre criando meus brinquedinhos e desafios pessoais, como fazer alguns bots, customização do sistema Android e scratting para pagar minhas contas.
-              Não tenho medo de abrir a documentação de uma linguagem aleatória no final de semana pra fazer alguma implementação "boba". Estudar paradigmas e estruturas de algoritmo que nunca vou usar novamente.
-              Me encontro encantado com o mundo das IA's e dando ênfase na matemática e estatística envolvida em treinamento das mesmas.
-          </p>
-          <p>
-            #DevOPS é cultura.
-          </p>
-        </DashboardInfo>
-      </DashboardContainer>  
-    );
+        ))}
+      </DashboardProjects>
+      <DashboardInfo>
+        <h2>Meu Nome é Pablo Bento Soares</h2>
+        <br />
+        <p>
+          Sou um desenvolvedor de software que teve o início da carreira na infraestrutura, experiência em scripts de automação, servidores e estruturas de escalabilidade e monitoramento, dando ênfase nas linguagens; Java, Python e JavaScript. Uma pessoa que ama a tecnologia desde que me entendo por gente, sempre criando meus brinquedinhos e desafios pessoais, como fazer alguns bots, customização do sistema Android e scripting para pagar minhas contas. Não tenho medo de abrir a documentação de uma linguagem aleatória no final de semana para fazer alguma implementação "boba". Estudo paradigmas e estruturas de algoritmo que nunca vou usar novamente. Me encontro encantado com o mundo das IA's e dou ênfase na matemática e estatística envolvida em treinamento das mesmas.
+        </p>
+        <p>
+          #DevOPS é cultura.
+        </p>
+      </DashboardInfo>
+    </DashboardContainer>
+  );
 };
 
 export default Dashboard;
